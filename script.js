16f14d40b64ea429a49c57eca63a03c2e20d5e8c
yskim308 <youngseokim7@gmail.com>
2024-02-12 20:58:33 +0900

    abstract grid function in javascript

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const grid = document.querySelector("#grid"); 
-
-
-let pixelCount = 4; 
-const gridWidth = grid.offsetWidth;
-console.log("width: " + gridWidth);
-
-let pixelSize = gridWidth/pixelCount; 
-console.log("pixel size: " + pixelSize);
-for (let a = 0; a < pixelCount; a++){
-    for (let b = 0; b < pixelCount; b++){
-        const square = document.createElement("div"); 
-        square.className = "pixel";
-        square.style.width = pixelSize + "px";
-        square.style.height = pixelSize + "px";
-        grid.appendChild(square);   
+function generateGrid(pixelCount){
+    const grid = document.querySelector("#grid"); 
+    const gridWidth = grid.clientWidth;
+    let pixelSize = gridWidth/pixelCount; 
+    console.log(gridWidth); 
+    console.log(pixelSize);
+    for (let a = 0; a < pixelCount; a++){
+        for (let b = 0; b < pixelCount; b++){
+            const square = document.createElement("div"); 
+            square.className = "pixel";
+            square.style.width = pixelSize + "px";
+            square.style.height = pixelSize + "px";
+            grid.appendChild(square);   
+        }
     }
 }
+
+generateGrid(32); 
